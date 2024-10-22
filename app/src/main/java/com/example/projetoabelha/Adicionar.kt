3268f423b4f9d685725c1ce646a0fd982e54a68f
Maryanne Gomes <maryannegomes99@gmail.com>
2024-10-22 18:54:23 -0300

    tela de adicionar enxames

--- app/src/main/java/com/example/projetoabelha/Adicionar.kt
+++ app/src/main/java/com/example/projetoabelha/Adicionar.kt
@@ -8,25 +8,25 @@ import androidx.activity.enableEdgeToEdge
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
-class Enxames : ComponentActivity() {
+class Adicionar : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_enxames)
+        setContentView(R.layout.activity_adicionar)
 
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
-        val btAdicionar = findViewById<Button>(R.id.bt_adicionar)
+        val btAdicionar = findViewById<Button>(R.id.voltar)
         btAdicionar.setOnClickListener {
-            irParaTelaAdicionar()
+            irParaTelaEnxames()
         }
     }
 
-    private fun irParaTelaAdicionar() {
-        val telaAdicionar = Intent(this, Adicionar::class.java)
-        startActivity(telaAdicionar)
+    private fun irParaTelaEnxames() {
+        val telaEnxames = Intent(this, Enxames::class.java)
+        startActivity(telaEnxames)
     }
 }
