7c9ec6120780deb5c1e247d6d0133c962ec9fee2
Vinicius Chaves <49620365+vinchaves@users.noreply.github.com>
2024-10-26 18:33:39 -0300

    list

--- app/src/main/java/com/example/projetoabelha/Enchames.kt
+++ app/src/main/java/com/example/projetoabelha/Enchames.kt
@@ -2,4 +2,8 @@ package com.example.projetoabelha
 
 class Enchames(var especie:String,var origem:String,var descricao:String,var data:String,var imageId:Int) {
 
+    override fun toString(): String {
+        return "Enchames \n" +
+                "Especie: $especie - Origem: $origem\nDescricao: $descricao\nData($data)"
+    }
 }
