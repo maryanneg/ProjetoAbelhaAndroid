7d7274ee6cfd966ba949e22793850f59a07d8b6e
pedrovsfelix <pedrovitorsf@gmail.com>
2024-11-12 20:18:31 -0300

    :sparkles: feat: Cadastro de usuários.

--- app/src/main/java/com/example/projetoabelha/Cadastro.kt
+++ app/src/main/java/com/example/projetoabelha/Cadastro.kt
@@ -1,25 +1,70 @@
 package com.example.projetoabelha
 
+import android.content.ContentValues
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.Toast
 import androidx.activity.ComponentActivity
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import com.example.projetoabelha.databinding.ActivityCadastroBinding
+import com.example.projetoabelha.util.UserDatabaseHelper
 
 class Cadastro : ComponentActivity() {
+    private lateinit var dbHelper: UserDatabaseHelper
     private lateinit var binding: ActivityCadastroBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
+
         binding = ActivityCadastroBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        dbHelper = UserDatabaseHelper(this)
 
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+
+        val btnCadastrar = findViewById<Button>(R.id.salvar_cadastro)
+        btnCadastrar.setOnClickListener{
+            cadastrarUsuario()
+        }
+    }
+
+    private fun cadastrarUsuario() {
+        val db = dbHelper.writableDatabase
+
+        val name = findViewById<EditText>(R.id.nome_usuario).text.toString()
+        val password = findViewById<EditText>(R.id.password).text.toString()
+        val email = findViewById<EditText>(R.id.Email).text.toString()
+        val phone = findViewById<EditText>(R.id.phone_usuario).text.toString()
+        val birth = findViewById<EditText>(R.id.data_nascimento_usuario).text.toString()
+
+        val values = ContentValues().apply {
+            put("name", name)
+            put("password", password)
+            put("email", email)
+            put("phone", phone)
+            put("birth", birth)
+        }
+
+        val newRowId = db.insert(UserDatabaseHelper.TABLE_NAME, null, values)
+        if(newRowId != -1L) {
+            Toast.makeText(this, "Usuário foi cadastrado com sucesso.", Toast.LENGTH_SHORT).show()
+            retornar()
+        } else {
+            Toast.makeText(this, "Erro ao cadastrar usuário.", Toast.LENGTH_SHORT).show()
+        }
+    }
+
+    private fun retornar() {
+        val intent = Intent(this, Telalogin::class.java)
+        startActivity(intent)
     }
 }
