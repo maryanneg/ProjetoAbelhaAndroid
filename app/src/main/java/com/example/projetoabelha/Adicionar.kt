cc050123d601d5fc635c003721391f3fb98b02d3
Vinicius Chaves <49620365+vinchaves@users.noreply.github.com>
2024-11-14 22:08:51 -0300

    Add e detalhes

--- app/src/main/java/com/example/projetoabelha/Adicionar.kt
+++ app/src/main/java/com/example/projetoabelha/Adicionar.kt
@@ -1,12 +1,16 @@
 package com.example.projetoabelha
 
+import android.content.ContentValues
 import android.content.Intent
 import android.os.Bundle
 import android.widget.Button
+import android.widget.EditText
+import android.widget.Toast
 import androidx.activity.ComponentActivity
 import androidx.activity.enableEdgeToEdge
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.example.projetoabelha.util.UserDatabaseHelper
 
 class Adicionar : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -19,9 +23,41 @@ class Adicionar : ComponentActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
-        val btAdicionar = findViewById<Button>(R.id.voltar)
-        btAdicionar.setOnClickListener {
+
+        val btVoltar = findViewById<Button>(R.id.voltar)
+        btVoltar.setOnClickListener {
             irParaTelaEnxames()
+        }
+
+        val btAdicionar = findViewById<Button>(R.id.salvar)
+        btAdicionar.setOnClickListener {
+            cadastrarEnxame()
+        }
+    }
+
+    private fun cadastrarEnxame() {
+        val db = dbHelper.writableDatabase
+
+        val especie = findViewById<EditText>(R.id.especie).text.toString()
+        val origem = findViewById<EditText>(R.id.origem).text.toString()
+        val descricao = findViewById<EditText>(R.id.descricao).text.toString()
+        val dataDia = findViewById<EditText>(R.id.dataDia).text.toString()
+        val imagem = findViewById<EditText>(R.id.buttonSelectImage).text.toString() //imagem corrigir
+
+        val values = ContentValues().apply {
+            put("especie", especie)
+            put("origem", origem)
+            put("descricao", descricao)
+            put("dataDia", dataDia)
+            put("imagem", imagem)
+        }
+
+        val newRowId = db.insert(UserDatabaseHelper.TABLE_NAME, null, values)
+        if(newRowId != -1L) {
+            Toast.makeText(this, "Enxame foi cadastrado com sucesso.", Toast.LENGTH_SHORT).show()
+            irParaTelaEnxames()
+        } else {
+            Toast.makeText(this, "Erro ao cadastrar Enxame.", Toast.LENGTH_SHORT).show()
         }
     }
 
--- app/src/main/java/com/example/projetoabelha/Detalhes.kt
+++ app/src/main/java/com/example/projetoabelha/Detalhes.kt
@@ -1,20 +1,34 @@
 package com.example.projetoabelha
 
+import android.annotation.SuppressLint
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Button
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
 class Detalhes : AppCompatActivity() {
+    @SuppressLint("MissingInflatedId")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_detalhes)
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+
+        val btVoltar = findViewById<Button>(R.id.voltar_3)
+        btVoltar.setOnClickListener {
+            irParaTelaEnxames()
+        }
+    }
+    private fun irParaTelaEnxames() {
+        val telaEnxames = Intent(this, Enxames::class.java)
+        startActivity(telaEnxames)
     }
 }
