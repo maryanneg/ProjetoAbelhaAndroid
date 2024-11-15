83d0cb70d5702fed4ab18930529de2dbe36e1cab
pedrovsfelix <pedrovitorsf@gmail.com>
2024-11-14 21:17:37 -0300

    git commit -m ":sparkles: feat: Adicionar Enxames"

--- app/src/main/java/com/example/projetoabelha/Adicionar.kt
+++ app/src/main/java/com/example/projetoabelha/Adicionar.kt
@@ -1,27 +1,65 @@
 package com.example.projetoabelha
 
+import android.content.ContentValues
 import android.content.Intent
 import android.os.Bundle
 import android.widget.Button
+import android.widget.EditText
+import android.widget.Toast
 import androidx.activity.ComponentActivity
 import androidx.activity.enableEdgeToEdge
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.example.projetoabelha.util.EnxameDatabaseHelper
 
 class Adicionar : ComponentActivity() {
+
+    private lateinit var dbHelper : EnxameDatabaseHelper
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         setContentView(R.layout.activity_adicionar)
+
+        dbHelper = EnxameDatabaseHelper(this)
 
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
-        val btAdicionar = findViewById<Button>(R.id.voltar)
-        btAdicionar.setOnClickListener {
+
+        val btnCadastrar = findViewById<Button>(R.id.salvar)
+        btnCadastrar.setOnClickListener{
+            cadastrarEnxame()
+        }
+
+        val btnVoltar = findViewById<Button>(R.id.voltar)
+        btnVoltar.setOnClickListener {
             irParaTelaEnxames()
+        }
+    }
+
+    private fun cadastrarEnxame() {
+        val db = dbHelper.writableDatabase
+
+        val especie = findViewById<EditText>(R.id.especie).text.toString()
+        val origem = findViewById<EditText>(R.id.origem).text.toString()
+        val descricao = findViewById<EditText>(R.id.descricao).text.toString()
+        val data = findViewById<EditText>(R.id.data).text.toString()
+
+        val values = ContentValues().apply {
+            put("especie", especie)
+            put("origem", origem)
+            put("descricao", descricao)
+            put("data", data)
+        }
+
+        val newRowId = db.insert(EnxameDatabaseHelper.TABLE_NAME, null, values)
+        if(newRowId != -1L) {
+            Toast.makeText(this, "Enxame cadastrado com sucesso!!!", Toast.LENGTH_SHORT).show()
+            irParaTelaEnxames()
+        } else {
+            Toast.makeText(this, "Erro ao cadastrar enxame...", Toast.LENGTH_SHORT).show()
         }
     }
 
